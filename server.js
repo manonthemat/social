5b3fdb95fe94557054e25d432bf3b2fcbfa79e1d
manonthemat <empower@manonthemat.com>
2014-08-30 14:49:08 -0700

    refactoring the server part

--- server.js
+++ server.js
@@ -1,33 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Post = require('./models/post');
 
 var app = express();
 app.use(bodyParser.json());
+app.use(require('./controllers/api/posts'));
 
 // handles the root
 app.get('/', function(req, res) {
   res.sendfile('layouts/posts.html');
-});
-
-// handles HTTP GET requests to /api/posts
-app.get('/api/posts', function(req, res, next) {
-  Post.find().sort('-date').exec(function(err, posts) {
-    if(err) { return next(err); }
-    res.json(posts);
-  });
-});
-
-// handles HTTP POST requests to /api/posts
-app.post('/api/posts', function(req, res, next) {
-  var post = new Post({
-    username: req.body.username,
-    body: req.body.body
-  });
-  post.save(function(err, post) {
-    if(err) { return next(err); }
-    res.status(201).json(post);
-  });
 });
 
 app.listen(3000, function() {
